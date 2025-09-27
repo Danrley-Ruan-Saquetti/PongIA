fc810e04d6d9d75a34241ac95bff37506901077d
Dan Ruan <danrsaquetti@gmail.com>
2025-09-27 19:33:24 -0300

    feat: Carregando as redes neurais na instância do jogo na inicialização da partida

--- src/nn/ai-trainer.ts
+++ src/nn/ai-trainer.ts
@@ -2,32 +2,35 @@ import { MultiGameController } from "../game/multi-game-controller.js";
 import { GLOBALS } from "../globals.js";
 import { IObservable, ListenerHandler, Observer } from "../utils/observer.js";
 import { getPopulationStorage, savePopulation } from "../utils/population-io.js";
+import { NeuralNetwork } from "./core/neural-network.js";
 import { Population } from './core/population.js';
 import { GameNN } from './game-nn.js';
 
-type PopulationNNGameEvents = {
+type AITrainerEvents = {
   'game-stop': GameNN
   'next-generation': undefined
 }
 
-export class AITrainer extends MultiGameController<GameNN> implements IObservable<PopulationNNGameEvents> {
+export class AITrainer extends MultiGameController<GameNN> implements IObservable<AITrainerEvents> {
 
-  private observer: Observer<PopulationNNGameEvents>
+  private observer: Observer<AITrainerEvents>
+  private neuralNetworks: NeuralNetwork[] = []
 
   population!: Population
 
   constructor(
-    private tableWith: number,
-    private tableHeight: number
+    tableWith: number,
+    tableHeight: number
   ) {
-    super(GLOBALS.population.size / 2)
-    this.observer = new Observer<PopulationNNGameEvents>()
+    super(tableWith, tableHeight, GLOBALS.population.size / 2)
+    this.observer = new Observer<AITrainerEvents>()
 
     this.loadPopulation()
   }
 
   protected onAllGamesFinish() {
     this.population.nextGeneration(GLOBALS.evolution)
+    this.neuralNetworks = this.population.individuals.map(network => network)
 
     savePopulation(this.population)
 
@@ -37,18 +40,29 @@ export class AITrainer extends MultiGameController<GameNN> implements IObservabl
   }
 
   protected createInstanceGame() {
-    return new GameNN(this.tableWith, this.tableHeight)
+    const game = new GameNN(this.tableWith, this.tableHeight)
+
+    game.on('game/start', () => {
+      const [networkLeft] = this.neuralNetworks.splice(Math.floor(Math.random() * this.neuralNetworks.length), 1)
+      const [networkRight] = this.neuralNetworks.splice(Math.floor(Math.random() * this.neuralNetworks.length), 1)
+
+      game.setNeuralNetworkLeft(networkLeft)
+      game.setNeuralNetworkRight(networkRight)
+    })
+
+    return game
   }
 
   private loadPopulation() {
     this.population = AITrainer.getPopulation()
+    this.neuralNetworks = this.population.individuals.map(network => network)
   }
 
-  on<EventName extends keyof PopulationNNGameEvents>(event: EventName, handler: ListenerHandler<PopulationNNGameEvents[EventName]>) {
+  on<EventName extends keyof AITrainerEvents>(event: EventName, handler: ListenerHandler<AITrainerEvents[EventName]>) {
     return this.observer.on(event, handler)
   }
 
-  clearListener(event: keyof PopulationNNGameEvents, id: string) {
+  clearListener(event: keyof AITrainerEvents, id: string) {
     this.observer.clearListener(event, id)
   }
 
@@ -56,7 +70,7 @@ export class AITrainer extends MultiGameController<GameNN> implements IObservabl
     this.observer.clearAllListeners()
   }
 
-  clearListenersByEvent(event: keyof PopulationNNGameEvents) {
+  clearListenersByEvent(event: keyof AITrainerEvents) {
     this.observer.clearListenersByEvent(event)
   }
 
