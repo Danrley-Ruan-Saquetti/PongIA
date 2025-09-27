8b414c890388cd12b4ad9fb6ababe76dfd40051e
Dan Ruan <danrsaquetti@gmail.com>
2025-09-27 19:30:41 -0300

    fix: Corrigindo nome da propriedade de largura do parâmetro de dimensão da função `resizecanvas`

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,11 +2,11 @@ export function generateID() {
   return Math.random().toString(36).substring(2, 9)
 }
 
-export function resizeCanvas(canvas: HTMLCanvasElement, { weight = 100, height = 100 }) {
+export function resizeCanvas(canvas: HTMLCanvasElement, { width = 100, height = 100 }) {
   const scale = 1
 
-  canvas.style.width = weight + 'px'
+  canvas.style.width = width + 'px'
   canvas.style.height = height + 'px'
-  canvas.width = weight * scale
+  canvas.width = width * scale
   canvas.height = height * scale
 }
