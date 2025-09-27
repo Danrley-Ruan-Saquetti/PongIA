17e2bd9f580eff51a57014c6f4faba64d1376f5a
Dan Ruan <danrsaquetti@gmail.com>
2025-09-27 15:35:20 -0300

    feat: Criando controller para gerenciamento de multiplus jogos simultâneos

---
+++ src/game/multi-game-controller.ts
@@ -0,0 +1,61 @@
+import { Game } from '../game/game.js';
+import { ObjectPool } from '../utils/object-pool.js';
+
+export class MultiGameController<TGame extends Game> {
+
+  private gamePool: ObjectPool<TGame>
+  games: TGame[] = []
+
+  private countGamesRunning = 0
+
+  constructor(gameLength: number) {
+    this.gamePool = new ObjectPool<TGame>(
+      gameLength,
+      () => this.createGame(),
+      game => this.resetGame(game)
+    )
+  }
+
+  startGames() {
+    this.games.splice(0, this.games.length)
+
+    while (this.gamePool.available() > 0) {
+      const game = this.gamePool.acquire()
+
+      game.start()
+
+      this.games.push(game)
+      this.countGamesRunning++
+    }
+  }
+
+  private createGame() {
+    const game = this.createInstanceGame()
+
+    game.on('game/stop', () => {
+      this.gamePool.release(game)
+
+      this.countGamesRunning--
+
+      if (this.countGamesRunning == 0) {
+        this.onAllGamesFinish()
+      }
+    })
+
+    return game
+  }
+
+  protected onAllGamesFinish() { }
+
+  private resetGame(game: TGame) {
+    game.reset()
+  }
+
+  protected createInstanceGame(): TGame {
+    return new Game(0, 0) as TGame
+  }
+
+  getCountGamesRunning() {
+    return this.countGamesRunning
+  }
+}
