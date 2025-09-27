4423a0a0f57b33ae03149eb1f8c6b556351cfe08
Dan Ruan <danrsaquetti@gmail.com>
2025-09-27 19:29:25 -0300

    feat: Alterando inicialização de `GameNN` para não receber a rede neural como parâmetro do construtor

--- src/nn/game-nn.ts
+++ src/nn/game-nn.ts
@@ -5,18 +5,33 @@ import { PaddleNN } from './paddle-nn.js';
 
 export class GameNN extends Game {
 
+  protected networkLeft: NeuralNetwork
+  protected networkRight: NeuralNetwork
+
   constructor(
     width: number,
     height: number,
-    protected networkLeft: NeuralNetwork,
-    protected networkRight: NeuralNetwork
   ) {
     super(width, height)
   }
 
-  protected createPaddles() {
-    this.paddleLeft = new PaddleNN(15, 100, this.width, this.height, TableSide.LEFT, this.ball, this.networkLeft);
-    this.paddleRight = new PaddleNN(15, 100, this.width, this.height, TableSide.RIGHT, this.ball, this.networkRight);
+  protected loadPaddles() {
+    this.paddleLeft = new PaddleNN(15, 100, this.width, this.height, TableSide.LEFT, this.ball);
+    this.paddleRight = new PaddleNN(15, 100, this.width, this.height, TableSide.RIGHT, this.ball);
+  }
+
+  setNeuralNetworkLeft(network: NeuralNetwork) {
+    this.networkLeft = network
+    if (this.paddleLeft instanceof PaddleNN) {
+      this.paddleLeft.network = network
+    }
+  }
+
+  setNeuralNetworkRight(network: NeuralNetwork) {
+    this.networkRight = network
+    if (this.paddleRight instanceof PaddleNN) {
+      this.paddleRight.network = network
+    }
   }
 
   getState() {
