2c67fda9da88eac227e6e7bfab9b2aebab74d7bf
Dan Ruan <danrsaquetti@gmail.com>
2025-09-28 19:12:02 -0300

    feat: Gravando a data de atualização da população em localStorage

--- src/utils/population-io.ts
+++ src/utils/population-io.ts
@@ -2,9 +2,16 @@ import { GLOBALS } from "../globals.js"
 import { Population } from "../nn/core/population.js"
 
 export function savePopulation(population: Population) {
-  if (GLOBALS.storage.enable) {
-    localStorage.setItem('population', JSON.stringify(population.toJSON()))
+  if (!GLOBALS.storage.enable) {
+    return
   }
+
+  const updatedAt = new Date(Date.now())
+
+  console.log('Population saved', updatedAt.toLocaleString('pt-BR'))
+
+  localStorage.setItem('population', JSON.stringify(population.toJSON()))
+  localStorage.setItem('population.updated_at', JSON.stringify(updatedAt))
 }
 
 export function getPopulationStorage(): Population | null {
