b6a2d41c41c47411b0f3c2a411b684711aeae1fc
Dan Ruan <danrsaquetti@gmail.com>
2025-09-29 22:23:59 -0300

    feat: Adicionando método `Vector2D.approach` para aproximação de vetores com base em delta

--- src/utils/vector2d.ts
+++ src/utils/vector2d.ts
@@ -4,4 +4,18 @@ export class Vector2D {
     public x = 0,
     public y = 0
   ) { }
+
+  approach(target: { x: number, y: number }, delta: number) {
+    if (Math.abs(target.x - this.x) <= delta) {
+      this.x = target.x;
+    } else {
+      this.x += (target.x > this.x ? 1 : -1) * delta;
+    }
+
+    if (Math.abs(target.y - this.y) <= delta) {
+      this.y = target.y;
+    } else {
+      this.y += (target.y > this.y ? 1 : -1) * delta;
+    }
+  }
 }
