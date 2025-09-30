4bbcbbde19521e506962d78d11c9848202929233
Dan Ruan <danrsaquetti@gmail.com>
2025-09-29 22:24:56 -0300

    feat: Criando especificação de `Paddle` que sempre seguirá a bola - `PaddleBot`

---
+++ src/game/paddle-bot.ts
@@ -0,0 +1,30 @@
+import { Paddle } from './paddle.js'
+import { TableSide } from './types.js'
+
+export class PaddleBot extends Paddle {
+
+  update() {
+    super.update()
+
+    this.position.approach({ x: this.position.x, y: this.ball.position.y - (this.height / 2) }, this.speed)
+
+    if (this.position.y < 0) {
+      this.position.y = 0
+    } else if (this.position.y + this.height > this.tableHeight) {
+      this.position.y = this.tableHeight - this.height
+    }
+  }
+
+  protected recalculateDirectionSpeedBall() {
+    const speed = Math.sqrt(this.ball.speed.x * this.ball.speed.x + this.ball.speed.y * this.ball.speed.y)
+
+    const angle = (Math.random() * Math.PI / 2) - (Math.PI / 4)
+    this.ball.speed.y = speed * Math.sin(angle)
+
+    if (this.side === TableSide.LEFT) {
+      this.ball.speed.x = Math.abs(speed * Math.cos(angle))
+    } else {
+      this.ball.speed.x = -Math.abs(speed * Math.cos(angle))
+    }
+  }
+}
