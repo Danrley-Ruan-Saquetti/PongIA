5f083957b18083399fd0baef21c0f88a32314491
Danrley-Ruan-Saquetti <Danrley-Ruan-Saquetti@users.noreply.github.com>
2025-09-30 01:25:38 +0000

    deploy: 0fe1e0c2d606b8c4a2bb492d7ec03c797ec1be5d

---
+++ dist/player-bot.js
@@ -0,0 +1,18 @@
+import { GameView } from "./game/game-view.js";
+import { Game } from "./game/game.js";
+import { PaddleBot } from './game/paddle-bot.js';
+import { TableSide } from './game/types.js';
+import { GLOBALS } from "./globals.js";
+import { PaddlePlayer } from './player/paddle-player.js';
+import { resizeCanvas } from "./utils/utils.js";
+window.onload = app;
+function app() {
+    const canvasGame = document.getElementById("gameCanvas");
+    resizeCanvas(canvasGame, GLOBALS.game.table);
+    const game = new Game(canvasGame.width, canvasGame.height, new PaddlePlayer(10, 100, canvasGame.width, canvasGame.height, TableSide.LEFT, 'w', 's'), new PaddleBot(10, 100, canvasGame.width, canvasGame.height, TableSide.RIGHT));
+    const gameView = new GameView(canvasGame);
+    gameView.setGame(game);
+    game.start();
+    gameView.start();
+}
+//# sourceMappingURL=player-bot.js.map
