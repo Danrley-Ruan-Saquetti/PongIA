209a03d81da5269f6bc10cd05af8b72497c71999
Dan Ruan <danrsaquetti@gmail.com>
2025-10-08 22:29:16 -0300

    feat: Aplicando normalização em cada input do método `feedforward` para centralizar em -1 e 1

--- src/nn/paddle-nn.ts
+++ src/nn/paddle-nn.ts
@@ -19,13 +19,9 @@ export class PaddleNN extends Paddle {
   }
 
   update() {
-    const [up, down, stay] = this.network.feedforward([
-      this.position.y + (this.height / 2),
-      this.ball.position.x,
-      this.ball.position.y,
-      this.ball.speed.x,
-      this.ball.speed.y,
-    ])
+    super.update()
+
+    const [up, down, stay] = this.network.feedforward(this.getInputNormalized())
 
     if (up > down && up > stay) {
       this.moveUp()
@@ -33,5 +29,15 @@ export class PaddleNN extends Paddle {
     else if (down > up && down > stay) {
       this.moveDown()
     }
+  }
+
+  protected getInputNormalized() {
+    return [
+      ((this.position.y + this.height / 2) / this.tableHeight) * 2 - 1,
+      (this.ball.position.x / this.tableWidth) * 2 - 1,
+      (this.ball.position.y / this.tableHeight) * 2 - 1,
+      this.ball.speed.x / this.ball.MAX_SPEED.x * this.ball.MAX_MULTIPLIER,
+      this.ball.speed.y / this.ball.MAX_SPEED.y * this.ball.MAX_MULTIPLIER
+    ]
   }
 }
